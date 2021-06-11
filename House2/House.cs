8e7b56428ae28f8d66eadc968c6f191510dfba2c
Soumay kanti Das <anikds2859@outlook.com>
2021-06-11 17:25:27 +0600

    Add project files.

---
+++ House2/House.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace House2
+{
+    public class House
+    {
+        private double Area { get; set; }
+        private string Color { get; set; }
+        private string Color2 { get; set; }
+        private string Color3 { get; set; }
+
+        public House(double area)
+        {
+            Area = area;
+        }
+        public House()
+        {
+            Color = "Brown";
+            Color2 = "Green";
+            Color3 = "Yellow";
+        }
+        public void  showdata()
+        {
+            Console.WriteLine($"I am a house. My area is {Area} m2");
+            Console.WriteLine($"I have 3 door one is {Color} one is {Color2} And the other is {Color3}");
+        }
+            
+            
+
+    }
+}
---
+++ House2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace House2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            House house = new House();
+            house.showdata();
+        }
+    }
+}
